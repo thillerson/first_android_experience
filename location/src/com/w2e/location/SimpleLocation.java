c8a5c21be9979645db293285451449c9ae8527db
Tony Hillerson <tony.hillerson@effectiveui.com>
2009-03-29 16:06:11 -0600

    initial location example

---
+++ location/src/com/w2e/location/SimpleLocation.java
@@ -0,0 +1,13 @@
+package com.w2e.location;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SimpleLocation extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

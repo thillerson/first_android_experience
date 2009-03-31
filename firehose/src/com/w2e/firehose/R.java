62d5f45cbb523cfd2582c238d61074e7a91fa2bb
Tony Hillerson <tony.hillerson@effectiveui.com>
2009-03-30 23:50:01 -0700

    skinned app example. A terrible looking twitter client

--- firehose/src/com/w2e/firehose/R.java
+++ firehose/src/com/w2e/firehose/R.java
@@ -12,9 +12,18 @@ public final class R {
     }
     public static final class color {
         public static final int primary_black=0x7f040000;
+        public static final int window_title_background=0x7f040001;
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int btn_default=0x7f020000;
+        public static final int btn_default_normal=0x7f020001;
+        public static final int btn_default_normal_disable=0x7f020002;
+        public static final int btn_default_normal_disable_focused=0x7f020003;
+        public static final int btn_default_pressed=0x7f020004;
+        public static final int btn_default_selected=0x7f020005;
+        public static final int icon=0x7f020006;
+        public static final int list_item_background=0x7f020007;
+        public static final int list_item_background_gradient=0x7f020008;
     }
     public static final class id {
         public static final int reload_button=0x7f070000;
@@ -45,8 +54,14 @@ public final class R {
         public static final int url=0x7f050006;
     }
     public static final class style {
-        public static final int user_detail_label=0x7f060002;
-        public static final int user_login_label=0x7f060001;
-        public static final int user_name_title=0x7f060000;
+        public static final int TextAppearance=0x7f060000;
+        public static final int TextAppearance_WindowTitle=0x7f060001;
+        public static final int Theme_TwitterFirehose=0x7f060008;
+        public static final int Widget_Button=0x7f060004;
+        public static final int WindowTitle=0x7f060003;
+        public static final int WindowTitleBackground=0x7f060002;
+        public static final int user_detail_label=0x7f060007;
+        public static final int user_login_label=0x7f060006;
+        public static final int user_name_title=0x7f060005;
     }
 }
---
+++ firehose/src/com/w2e/firehose/activity/StyledActivity.java
@@ -0,0 +1,5 @@
+package com.w2e.firehose.activity;
+
+public class StyledActivity extends ListWithCustomListItem {
+
+}
